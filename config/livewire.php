71bc0529013f0fc5271e9b10facf5c4496673a47
Harsh <agarwalhasrh414@gmail.com>
2021-06-18 19:28:12 +0530

    changes

--- config/livewire.php
+++ config/livewire.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'asset_url' => 'http://searchmovieshow.herokuapp.com',
+    'asset_url' => url('/'),
 
     /*
     |--------------------------------------------------------------------------
